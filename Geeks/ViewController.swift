aeb06466dddc8fd9707153b7c67acd3bc4e7df64
d1onnisss <lim_al@auca.kg>
2024-08-12 19:40:14 +0600

    Initial Commit

---
+++ Geeks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geeks
+//
+//  Created by Alexey Lim on 12/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
